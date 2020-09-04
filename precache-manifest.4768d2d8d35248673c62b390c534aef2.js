4f478a11137e3151570a92b6ffe5dd60c03953fd
whj <739960552@qq.com>
2020-09-05 02:10:58 +0800

    deploy

---
+++ precache-manifest.4768d2d8d35248673c62b390c534aef2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c0116e1f49b65542b8",
+    "url": "/after-tea-bill-website/css/app.2b840e9d.css"
+  },
+  {
+    "revision": "a8d0885922d17f53db00166103e7a229",
+    "url": "/after-tea-bill-website/img/qrcode.png"
+  },
+  {
+    "revision": "09b3e6b0406f6f905a4660263f4eb8aa",
+    "url": "/after-tea-bill-website/index.html"
+  },
+  {
+    "revision": "55c0116e1f49b65542b8",
+    "url": "/after-tea-bill-website/js/app.ceb54231.js"
+  },
+  {
+    "revision": "b98c4f38ecd21122760c",
+    "url": "/after-tea-bill-website/js/chunk-vendors.c5c643ca.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/after-tea-bill-website/manifest.json"
+  },
+  {
+    "revision": "a8d0885922d17f53db00166103e7a229",
+    "url": "/after-tea-bill-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/after-tea-bill-website/robots.txt"
+  }
+]);
